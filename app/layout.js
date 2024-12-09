408746abf87b8372f947b86aea466735173745ee
mykhbas <wolfbaszxc@gmail.com>
2024-12-09 12:18:51 +0700

    title

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,10 @@
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <title>Dota2Hero</title> 
+      </head>
       <body>{children}</body>
     </html>
   );
